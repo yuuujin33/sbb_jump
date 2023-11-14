fb250c927a5f8bf79b007b89ab9c45f461734055
yuuujin33 <yuuujin9922@gmail.com>
2023-11-14 12:22:22 +0900

    init

---
+++ src/main/java/com/mysite/sbb_jump/SbbJumpApplication.java
@@ -0,0 +1,13 @@
+package com.mysite.sbb_jump;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbbJumpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbbJumpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mysite/sbb_jump/SbbJumpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mysite.sbb_jump;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbbJumpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
